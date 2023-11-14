d57314118ab1ac1c9984dd99266f74d41daa471a
Prashu <Prashanth.P@stackroute.in>
2023-11-14 12:41:01 +0530

    Initial commit

---
+++ ApiGateway/src/main/java/com/stackroute/ApiGateway/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.stackroute.ApiGateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Priyanshu Singh
+ */
+
+/*
+Add configuration annotation
+ */
+public class GatewayConfig {
+
+   /*
+   * create a method to Route request to  http://localhost:8090 when /api/v1/** is hit
+    */
+}
+
